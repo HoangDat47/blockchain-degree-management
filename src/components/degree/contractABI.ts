9b50dd0617ccebec2109f715ce7fbee084248b30
HoangDat47 <dat47707@gmail.com>
2025-02-24 22:04:51 +0700

    Add contract ABI for degree management functions

---
+++ src/components/degree/contractABI.ts
@@ -0,0 +1,137 @@
+export const contractABI = [
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "id",
+          type: "uint256",
+        },
+        {
+          internalType: "string",
+          name: "studentName",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "email",
+          type: "string",
+        },
+        {
+          internalType: "uint256",
+          name: "phone",
+          type: "uint256",
+        },
+        {
+          internalType: "string",
+          name: "degreeName",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "ifpsHash",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "ifpsUrl",
+          type: "string",
+        },
+      ],
+      name: "addRecord",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "provider",
+          type: "address",
+        },
+      ],
+      name: "authorizeProvider",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      stateMutability: "nonpayable",
+      type: "constructor",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "id",
+          type: "uint256",
+        },
+      ],
+      name: "getDegreeRecords",
+      outputs: [
+        {
+          components: [
+            {
+              internalType: "uint256",
+              name: "id",
+              type: "uint256",
+            },
+            {
+              internalType: "string",
+              name: "studentName",
+              type: "string",
+            },
+            {
+              internalType: "string",
+              name: "email",
+              type: "string",
+            },
+            {
+              internalType: "uint256",
+              name: "phone",
+              type: "uint256",
+            },
+            {
+              internalType: "string",
+              name: "degreeName",
+              type: "string",
+            },
+            {
+              internalType: "string",
+              name: "ifpsHash",
+              type: "string",
+            },
+            {
+              internalType: "string",
+              name: "ifpsUrl",
+              type: "string",
+            },
+            {
+              internalType: "uint256",
+              name: "timestamp",
+              type: "uint256",
+            },
+          ],
+          internalType: "struct DegreeRecords.Degree[]",
+          name: "",
+          type: "tuple[]",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getOwner",
+      outputs: [
+        {
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+  ];
