2943f1ec1cbb68d46bf4bc38ecc6383fd9312930
HoangDat47 <dat47707@gmail.com>
2025-02-24 22:36:11 +0700

    Refactor file upload logic to return CID instead of signed URL

--- src/app/api/files/route.ts
+++ src/app/api/files/route.ts
@@ -12,11 +12,11 @@ export async function POST(request: NextRequest) {
     const data = await request.formData();
     const file: File | null = data.get("file") as unknown as File;
     const { cid } = await pinata.upload.file(file)
-    const url = await pinata.gateways.createSignedURL({
-			cid: cid,
-			expires: 3600,
-		});
-    return NextResponse.json(url, { status: 200 });
+    // const url = await pinata.gateways.createSignedURL({
+		// 	cid: cid,
+		// 	expires: 3600,
+		// });
+    return NextResponse.json(cid, { status: 200 });
   } catch (e) {
     console.log(e);
     return NextResponse.json(
