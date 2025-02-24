8c1aad1cec9850e1e9f32cd5dfd1ecd600b3a1c7
HoangDat47 <dat47707@gmail.com>
2025-02-24 22:05:01 +0700

    Add Degree component for managing degree records and wallet integration

---
+++ src/components/degree/degree.tsx
@@ -0,0 +1,269 @@
+"use client";
+import { useEffect, useState, useRef } from "react";
+import { ethers } from "ethers";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { contractABI } from "./contractABI";
+
+export default function Degree() {
+  const [provider, setProvider] =
+    useState<ethers.providers.Web3Provider | null>(null);
+  const [signer, setSigner] = useState<ethers.Signer | null>(null);
+  const [contract, setContract] = useState<ethers.Contract | null>(null);
+  const [account, setAccount] = useState<string | null>(null);
+  const [isOwner, setIsOwner] = useState<boolean | null>(null);
+  const [providerAddress, setProviderAddress] = useState<string>("");
+  const [id, setID] = useState<string>("");
+  const [degreeRecords, setDegreeRecords] = useState<any[]>([]);
+  const [studentName, setStudentName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [degreeName, setDegreeName] = useState<string>("");
+  const [ifpsHash, setIfpsHash] = useState<string>("");
+  const [ifpsUrl, setIfpsUrl] = useState<string>("");
+  const [file, setFile] = useState("");
+  const [url, setUrl] = useState("");
+  const [uploading, setUploading] = useState(false);
+  const inputFile = useRef(null);
+
+  const contractAddress = "0xd28d7c26e03e539b63570b1f17c21634883d62ae";
+
+  useEffect(() => {
+    const connectWallet = async () => {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        setProvider(provider);
+        setSigner(signer);
+
+        const accountAddress = await signer.getAddress();
+        setAccount(accountAddress);
+
+        const contractInstance = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        setContract(contractInstance);
+
+        const ownerAddress = await contractInstance.getOwner();
+        setIsOwner(accountAddress.toLowerCase() === ownerAddress.toLowerCase());
+      } catch (error) {
+        console.error("Error connecting to wallet: ", error);
+      }
+    };
+
+    connectWallet();
+  }, []);
+
+  const fetchDegreeRecords = async (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!contract) {
+      console.error("Hợp đồng chưa được khởi tạo");
+      return;
+    }
+
+    try {
+      const records = await contract.getDegreeRecords(id);
+      console.log("records", records);
+      setDegreeRecords(records);
+    } catch (error) {
+      console.error("Không thể lấy thông tin bằng cấp", error);
+    }
+  };
+
+  const addRecord = async (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!contract) {
+      console.error("Hợp đồng chưa được khởi tạo");
+      return;
+    }
+
+    try {
+      const tx = await contract.addRecord(
+        id,
+        studentName,
+        email,
+        "0",
+        degreeName,
+        ifpsHash,
+        ifpsUrl
+      );
+      await tx.wait();
+      fetchDegreeRecords(event);
+      alert("Đã thêm bằng cấp thành công");
+    } catch (error) {
+      console.error("Không thể thêm bằng cấp", error);
+    }
+  };
+
+  const authorizeProvider = async (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!contract) {
+      console.error("Hợp đồng chưa được khởi tạo");
+      return;
+    }
+
+    if (isOwner) {
+      try {
+        const tx = await contract.authorizeProvider(providerAddress);
+        await tx.wait();
+        alert(`Đã ủy quyền nhà cung cấp: ${providerAddress} thành công`);
+      } catch (error) {
+        console.error(
+          "Chỉ có người sở hữu hợp đồng mới được uỷ quyền nhà cung cấp khác",
+          error
+        );
+      }
+    } else {
+      alert("Chỉ có người sở hữu hợp đồng mới được uỷ quyền nhà cung cấp khác");
+    }
+  };
+
+  const uploadFile = async () => {
+    try {
+      setUploading(true);
+      const data = new FormData();
+      data.set("file", file);
+      const uploadRequest = await fetch("/api/files", {
+        method: "POST",
+        body: data,
+      });
+      const signedUrl = await uploadRequest.json();
+      setUrl(signedUrl);
+      setUploading(false);
+    } catch (e) {
+      console.log(e);
+      setUploading(false);
+      alert("Trouble uploading file");
+    }
+  };
+
+  const handleChange = (e: any) => {
+    setFile(e.target.files[0]);
+  };
+
+  return (
+    <div>
+      <h1>Quản lý bằng cấp</h1>
+      <p>Tài khoản kết nối: {account}</p>
+      {isOwner && <p>Bạn là người sở hữu hợp đồng này.</p>}
+
+      <form onSubmit={fetchDegreeRecords}>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="id">ID</Label>
+          <Input
+            type="text"
+            placeholder="Nhập ID"
+            value={id}
+            onChange={(e) => setID(e.target.value)}
+          />
+          <Button className="bg-white text-black p-2 rounded-md" type="submit">
+            Lấy thông tin bằng cấp
+          </Button>
+        </div>
+      </form>
+
+      <form onSubmit={addRecord}>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="id">Họ và tên</Label>
+          <Input
+            type="text"
+            placeholder="Họ và tên"
+            value={studentName}
+            onChange={(e) => setStudentName(e.target.value)}
+          />
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="id">Email</Label>
+          <Input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        {/* <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="id">Số điện thoại</Label>
+          <Input
+            type="text"
+            placeholder="Số điện thoại"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </div> */}
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="id">Tên bằng cấp</Label>
+          <Input
+            type="text"
+            placeholder="Tên bằng cấp"
+            value={degreeName}
+            onChange={(e) => setDegreeName(e.target.value)}
+          />
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="picture">Chọn tệp</Label>
+      <Input id="picture" type="file" accept=".png, .jpg, .pdf" ref={inputFile} onChange={handleChange} />
+      <Button className="bg-white text-black p-2 rounded-md" disabled={uploading} onClick={uploadFile}>
+      {uploading ? "Uploading..." : "Upload"}</Button>
+      {url && <a href={url} className="underline" target="_blank">{url}</a>}
+    </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="id">IFPS Hash</Label>
+          <Input
+            type="text"
+            placeholder="IFPS Hash"
+            value={ifpsHash}
+            onChange={(e) => setIfpsHash(e.target.value)}
+          />
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="id">IFPS URL</Label>
+          <Input
+            type="text"
+            placeholder="IFPS URL"
+            value={ifpsUrl}
+            onChange={(e) => setIfpsUrl(e.target.value)}
+          />
+        </div>
+        <Button className="bg-white text-black p-2 rounded-md" type="submit">
+          Thêm bằng cấp
+        </Button>
+      </form>
+
+      <form onSubmit={authorizeProvider}>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="id">Ủy quyền nhà cung cấp</Label>
+          <Input
+            type="text"
+            placeholder="Địa chỉ nhà cung cấp"
+            value={providerAddress}
+            onChange={(e) => setProviderAddress(e.target.value)}
+          />
+          <Button className="bg-white text-black p-2 rounded-md" type="submit">
+            Ủy quyền nhà cung cấp
+          </Button>
+        </div>
+      </form>
+
+      <div>
+        <h2>Thông tin bằng cấp</h2>
+        <ul>
+          {degreeRecords.map((record: any, index: number) => (
+            <li key={index}>
+              <p>ID: {record.id.toString()}</p>
+              <p>Họ và tên: {record.studentName}</p>
+              <p>Email: {record.email}</p>
+              <p>Tên bằng cấp: {record.degreeName}</p>
+              <p>IFPS Hash: {record.ifpsHash}</p>
+              <p>IFPS URL: {record.ifpsUrl}</p>
+              <p>Thời gian: {record.timestamp.toString()}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
