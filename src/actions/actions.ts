554b0c4650fd08fa4aa550ec97f8d3565a946a57
HoangDat47 <dat47707@gmail.com>
2025-03-10 21:29:58 +0700

    Remove unused putStudentDB action and clean up imports in actions.ts

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,14 +1,8 @@
 "use server";
 
-import { Students } from "@/types";
-import {getStudent, putStudent } from "./data";
+import {getStudent} from "./data";
 
 export const getStudentFromDB = async (id: number) => {
   const res = await getStudent(id);
-  return res;
-};
-
-export const putStudentDB = async (data: Students) => {
-  const res = await putStudent(data);
   return res;
 };
