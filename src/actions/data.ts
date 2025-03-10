4ef120265ee3810c991505e7d6f55ddec58c4027
HoangDat47 <dat47707@gmail.com>
2025-03-10 21:30:05 +0700

    Add actions to fetch all students and a single student by ID from the API

---
+++ src/actions/data.ts
@@ -0,0 +1,36 @@
+import "server-only";
+
+export const getStudents = async () => {
+  try {
+    const response = await fetch(
+      `${process.env.AWS_API_URL}/students`,
+      {
+        cache: "no-store",
+      }
+    ).then(async function (res) {
+      const status = res.status;
+      const data = await res.json();
+      return { data, status };
+    });
+    return response;
+  } catch (error) {
+    console.log("Error: ", error);
+    throw new Error("Failed to fetch Students.");
+  }
+};
+
+export const getStudent = async (id: number) => {
+  try {
+    const response = await fetch(
+      `${process.env.AWS_API_URL}/students/${id}`
+    ).then(async function (res) {
+      const status = res.status;
+      const data = await res.json();
+      return { data, status };
+    });
+    return response;
+  } catch (error) {
+    console.log("Error: ", error);
+    throw new Error("Failed to fetch Student.");
+  }
+};
